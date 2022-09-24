86495634e79868423244d42de28862d2e8ad6f3e
bozzelliandrea <bozzelliandrea@yahoo.it>
2022-09-24 16:52:17 +0200

    feat(auth): complete api for auth flow
    TODO:
    * switch to database persisted data instead of runtime map

--- packages/auth/_auth.js
+++ packages/auth/_auth.js
@@ -1,6 +1,7 @@
 const fakeMongo = new Map();
 const core = require('../core/index');
-const {hashPassword} = require('./_auth-utils');
+const {hashPassword, validatePassword, getToken} = require('./_auth-utils');
+const {decode} = require('jsonwebtoken');
 
 async function register(username, email, password) {
     if (!email || !username || !password)
@@ -21,13 +22,34 @@ async function register(username, email, password) {
         throw new core.appError(core.httpCode.UNPROCESSABLE_ENTITY, "Failed to save user");
     }
 
-    return user;
+    return new core.appResponse(core.httpCode.CREATED, {username}, getToken(username, "USER"));
 }
 
-async function login() {
+async function login(username, password) {
+    if (!username || !password)
+        throw new core.appError(core.httpCode.BAD_REQUEST, "Login required parameter cannot be null");
+
+    const user = fakeMongo.get(username);
+
+    if (!user)
+        throw new core.appError(core.httpCode.UNAUTHORIZED, "The user does not exist")
+
+    if(!validatePassword(user.hash, password, user.salt))
+        throw new core.appError(core.httpCode.UNAUTHORIZED, "Wrong password")
+
+    return new core.appResponse(null, null, getToken(username, "USER"));
 }
 
-async function refresh() {
+async function refresh(token) {
+    if (!token)
+        throw new core.appError(core.httpCode.BAD_REQUEST, "Token not found");
+
+    const decoded = decode(token);
+
+    if (!fakeMongo.get(decoded.username))
+        throw new core.appError(core.httpCode.UNAUTHORIZED, "The user does not exist")
+
+    return new core.appResponse(null, {username: decoded.username}, getToken(decoded.username, decoded.role));
 }
 
 module.exports = {
--- packages/core/error-handler.middleware.js
+++ packages/core/error-handler.middleware.js
@@ -1,9 +1,9 @@
 module.exports = function (err, req, res, next) {
     console.error(err);
 
-    res.status(err.code).send({
-        code: err.code,
-        message: err.message,
+    res.status(err.code || 500).send({
+        code: err.code || 500,
+        message: err.message || 'Internal Server Error',
         description: err.description
     })
 }
